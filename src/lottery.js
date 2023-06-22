009003e5ea4eae8d6c2c744cde7bb83c36f962e4
mamismeda <amamismeda@gmail.com>
2023-06-22 14:16:14 +0400

    end of simple lottery project with solidity

---
+++ src/lottery.js
@@ -0,0 +1,61 @@
+import web3 from "./web3";
+
+const address = "0x527b2ac548281DdBfbd942cd724965e377449e4f";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+const web3Contract = new web3.eth.Contract(abi, address);
+
+export default web3Contract;
